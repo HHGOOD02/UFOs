b96a6d8b26a62ef0b2e2449b925a9a180e280606
HHGOOD02 <henry.hawgood@gmail.com>
2022-07-11 03:24:19 -0500

    initial commit

---
+++ static/js/app.js
@@ -0,0 +1,18 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+function listLoop(userList) {
+    for (var i = 0; i < userList.length; i++) {
+      console.log(userList[i]);
+    }
+ }
+
+ function buildTable(data) {
+    tbody.html("");
+    data.foreach((dataRow) => {
+        let row = tbody.append("tr");
+    });
+}
