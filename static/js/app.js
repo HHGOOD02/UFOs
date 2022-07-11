34ed6f3bd8359d27d6dcd8875e3fe15bafed03d0
HHGOOD02 <henry.hawgood@gmail.com>
2022-07-11 05:03:44 -0500

    functionality

--- static/js/app.js
+++ static/js/app.js
@@ -14,5 +14,9 @@ function listLoop(userList) {
     tbody.html("");
     data.foreach((dataRow) => {
         let row = tbody.append("tr");
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+        });
     });
 }
